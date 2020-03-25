520c2669331c95989f873f00ec3c9cf0f96d9df1
Scott Gerlach <scott.gerlach@stackhawk.com>
2020-03-25 14:29:28 -0600

    no debug

--- src/vuln_django/settings.py
+++ src/vuln_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mnu_5q!2b^cesn9w+gfulw2d4ogj5o^s#54anr3+w!k_xkhcti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', 'vuln-django']
 

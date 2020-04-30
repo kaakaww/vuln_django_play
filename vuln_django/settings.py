e3d3be957af2b937d622df63b88550f2c370cfc8
kberg <kc@stackhawk.com>
2020-04-30 11:03:29 -0600

    referer madness

--- vuln_django/settings.py
+++ vuln_django/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+SECURE_REFERRER_POLICY = 'same-origin'
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
